7a5abdae4a6f24c3502d8a316a2089e0c8332bb3
Kwartz <43706541+kwartz-2002288@users.noreply.github.com>
2021-05-08 18:08:48 +0200

    Add files via upload

--- readKeysLib.py
+++ readKeysLib.py
@@ -1,26 +1,35 @@
-# create an APIkey dictionnary {user:APIkey, ... } 
+import os,json
+
+# create an APIkey dictionnary {user:APIkey, ... }
 # from a textfile data
 # name1 APIkey1
 # name2 APIkey2 etc...
 
-def getKeyDict(file_name):
-    " create a Key dictionnary {name:key, ... } from a textfile: name,key\n etc "
-    myFile=open(file_name,'r')
-    dict={}
-    for line in myFile:
-        L=line.rstrip('\n').split(',')
-        dict[L[0]] = L[1]
-    myFile.close()
-    return dict
+nodeName = os.uname().nodename
+if nodeName == 'raspberrypi':
+    repertory = '/home/jpr/Documents/torn/files/' # Rapsberry path
+elif nodeName == 'iMac-JP.local' or nodeName == 'MacBook-JP.local':
+    repertory = '/Users/jpr/Dropbox/torn/files/' # Mac path
+else:
+    raise Exception('Unknown computer')
 
 def getDicts():
-    " get the two dictionnaries needed "
+    " get the dictionnaries needed from editable json files"
+    with open(repertory+'APIKeys.txt') as f:
+        APIKeys = json.load(f)
+    with open(repertory+'sheetKeys.txt') as f:
+        sheetKeys = json.load(f)
+    sheetKeys['rep']=repertory
+    return APIKeys, sheetKeys
 
-    repertory = '/home/jpr/Documents/torn/files/'  # Rapsberry path
-    #repertory = '/Users/jpr/Dropbox/JP-Manu/torn/files/' # iMac or MacBook path
+def getLentStocks():
+    " get the lent stocks information "
+    with open(repertory+'lentStocks.txt','r') as f:
+        dict = json.load(f)
+    return dict
 
-    APIKey = getKeyDict(repertory+'APIKeys.txt')
-    sheetKey = getKeyDict(repertory+'sheetKeys.txt')
-    sheetKey['rep']=repertory
-    return APIKey, sheetKey
-    
+def getYATAtargets():
+    " get the targets exported by YATA"
+    with open(repertory+'targets.json','r') as f:
+        dict = json.load(f)
+    return dict
