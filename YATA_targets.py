f7b184bfe6ac9cd44f1fa8a917757fc02c387afa
kwartz-2002288 <jp.roubin@gmail.com>
2022-09-28 16:51:11 +0200

    Modified to write all targets with only one update instruction.

--- YATA_targets.py
+++ YATA_targets.py
@@ -1,3 +1,5 @@
+# Modified to write the whole spreadsheet zone in one instruction
+
 import datetime
 import requests, gspread, string
 from oauth2client.service_account import ServiceAccountCredentials
@@ -7,7 +9,7 @@ import readKeysLib
 # Ce fichier targets.json doit être placé dans le dossier ..../torn/files !
 
 # APIKeys and sheetKeys are saved in files in an external repertory see the module readKeysLib
-APIKey_dict,sheetKey_dict = readKeysLib.getDicts()
+APIKey_dict, sheetKey_dict, nodeName = readKeysLib.getDicts()
 repertory=sheetKey_dict['rep']
 # Get authorization for gspread and open worksheet
 scope = ['https://spreadsheets.google.com/feeds']
@@ -27,18 +29,21 @@ print('-> sheet opened')
 # Lecture du fichier
 targets_dict = readKeysLib.getYATAtargets()
 print(f'-> {len(targets_dict)} targets found')
-print(targets_dict)
+# print(targets_dict)
 #print(f'total_ws : {total_ws:.2f}, model: {total_ws_model:.2f}, delta: {delta:.2f} %')
 # for id,player in targets_dict.items():
 # {"133606":{"name":"WhiteHart","life_current":1902,"life_maximum":6337,"status_description":"Okay","status_details":"","status_color":"green","status_state":"Okay","status_until":0,"level":87,"last_action_relative":"43 days ago","last_action_timestamp":1612282620,"last_action_status":"Offline","update_timestamp":1616014627,"last_attack_attacker":true,"last_attack_timestamp":1614477375,"note":"*****","color":1,"result":"Attacked","fair_fight":2.97,"flat_respect":4.070588235294117,"base_respect":1.3705684293919587,"win":1},
 
-line = 2
+L = []
 for id,player in targets_dict.items():
-    if line < 9 :
-        L = [id,player["name"],player["color"],player["fair_fight"],player["flat_respect"],player["last_action_relative"],player["note"]]
-        ws.update('A'+str(line),[L])
-    line+=1
-
+    L.append([id,player["name"],player["color"],player["fair_fight"],player["flat_respect"],player["last_action_relative"],player["note"]])
+print(L[0])
+print('...')
+print(L[-1])
+zone = 'A2:'+'G'+str(1+len(L))
+print('zone spreadsheet to be filled : ',zone)
+ws.update(zone,L)
+# print(L)
 # ws.update_cell(10,1,currentDateGspread)
 # ws.update_cell(11,1,currentDateGspread+0.5)
 # previousDateGspreadStr = ws.cell(12,1).value
